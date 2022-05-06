4eb35d0b3d37e1e16c5086bbe742f546b585b9e4
Dambi <matan.wink@gmail.com>
2022-05-06 16:48:18 +0300

    Opened a project in C++11

---
+++ cmake-build-debug/CMakeFiles/Matam_HW2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matam_HW2.dir/main.cpp.obj"
+  "Matam_HW2.exe"
+  "Matam_HW2.exe.manifest"
+  "Matam_HW2.pdb"
+  "libMatam_HW2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matam_HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
