19050a7348207393fd83198e5879d6cc1a1e3a40
Dambi <matan.wink@gmail.com>
2022-05-06 20:42:19 +0300

    Declared all Player functions

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,54 @@
-//
-// Created by PC on 6/5/2022.
-//
+//TODO: figure out strings as arguments to functions.
 
 #include "Player.h"
 
+Player::Player(char* Name, int MaxHP, int Force){
+    name = Name;
+    maxHP = MaxHP;
+    HP = maxHP;
+    force = Force;
+    Level = 1;
+    coins = 0;
+}
+
+Player::Player(const Player &) {} //TODO: implement
+
+Player::~Player() {} //TODO: implement
+
+void Player::printInfo() {
+
+}
+
+
+int Player::getAttackStrength() {
+    return 0;
+}
+
+bool Player::pay(int amount) {
+    return false;
+}
+
+void Player::addCoins() {
+
+}
+
+bool Player::isKnockedOut() {
+    return false;
+}
+
+void Player::damage(int amount) {
+
+}
+
+void Player::heal(int amount) {
+
+}
+
+void Player::buff(int amount) {
+
+}
+
+int Player::getLevel() {
+    return 0;
+}
 
--- Player.h
+++ Player.h
@@ -11,9 +11,19 @@ class Player {
     int Level;
     int force, maxHP, HP, coins;
 public:
-    Player(int Force); //constructor
+    Player(char* Name, int MaxHP = 100, int Force = 5); //constructor //TODO: conventions for constructor parameters
     Player(const Player&); //copy constructor
     ~Player(); // destructor
+
+    void printInfo();
+    int getLevel();
+    void buff(int amount);
+    void heal(int amount);
+    void damage(int amount);
+    bool isKnockedOut();
+    void addCoins();
+    bool pay(int amount);
+    int getAttackStrength();
 };
 
 
