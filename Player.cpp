13204a908556921bff8c89afafd868e6e92184a5
Dambi <matan.wink@gmail.com>
2022-05-06 21:09:58 +0300

    Finished writing all Player functions besides copy constructor and deconstructor. Still some tidying up to do.

--- Player.cpp
+++ Player.cpp
@@ -1,13 +1,30 @@
 //TODO: figure out strings as arguments to functions.
+//TODO: edge cases on all functions: inputs 0, NULL, negative. etc.
 
 #include "Player.h"
+#include "utilities.h"
+#include <cassert> //TODO: use "assert" instead? which library are we allowed to use?
+
+
+static int fill(int threshold, int attribute, int amount, int direction){ //TODO: better parameter names
+    assert(amount >= 0); //TODO: maybe handle edge cases instead of asserting?
+    int change = amount*direction;
+    if ((attribute + change)*direction > threshold)
+    {
+        return threshold;
+    }
+    else
+    {
+        return attribute+change;
+    }
+}
 
 Player::Player(char* Name, int MaxHP, int Force){
     name = Name;
     maxHP = MaxHP;
     HP = maxHP;
     force = Force;
-    Level = 1;
+    level = 1;
     coins = 0;
 }
 
@@ -15,40 +32,75 @@ Player::Player(const Player &) {} //TODO: implement
 
 Player::~Player() {} //TODO: implement
 
-void Player::printInfo() {
-
+void Player::printInfo() const {
+    printPlayerInfo(name, level, force, HP, coins);
 }
 
-
-int Player::getAttackStrength() {
-    return 0;
+void Player::levelUP() {
+    if (level >= 10)
+    {
+        return;
+    }
+    else
+    {
+        level++;
+    }
 }
 
-bool Player::pay(int amount) {
-    return false;
-}
-
-void Player::addCoins() {
-
-}
-
-bool Player::isKnockedOut() {
-    return false;
-}
-
-void Player::damage(int amount) {
-
-}
-
-void Player::heal(int amount) {
-
+int Player::getLevel() const {
+    return level;
 }
 
 void Player::buff(int amount) {
-
+    //TODO: handle force max size
+    force++;
 }
 
-int Player::getLevel() {
-    return 0;
+void Player::heal(int amount) {
+    HP = fill(maxHP, HP, amount, UPPER_BOUND);
 }
+
+void Player::damage(int amount) {
+    HP = fill(0, HP, amount, LOWER_BOUND);
+}
+
+bool Player::isKnockedOut() const {
+    if (HP <= 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void Player::addCoins(int amount) {
+    assert(amount > 0);
+    coins+=amount;
+}
+
+bool Player::pay(int amount) {
+    if (coins < amount)
+    {
+        return false;
+    }
+    else
+    {
+        coins-=amount;
+        return true;
+    }
+}
+
+int Player::getAttackStrength() const {
+    return level+force;
+}
+
+
+
+
+
+
+
+
+
+
 
--- Player.h
+++ Player.h
@@ -3,27 +3,30 @@
 #ifndef MATAM_HW2_PLAYER_H
 #define MATAM_HW2_PLAYER_H
 
+#define UPPER_BOUND 1
+#define LOWER_BOUND -1
 
 class Player {
     //"private:" is automatic with classes
 
     char* name;
-    int Level;
+    int level;
     int force, maxHP, HP, coins;
 public:
     Player(char* Name, int MaxHP = 100, int Force = 5); //constructor //TODO: conventions for constructor parameters
     Player(const Player&); //copy constructor
     ~Player(); // destructor
 
-    void printInfo();
-    int getLevel();
+    void printInfo() const;
+    int getLevel() const;
+    void levelUP();
     void buff(int amount);
     void heal(int amount);
     void damage(int amount);
-    bool isKnockedOut();
-    void addCoins();
+    bool isKnockedOut() const;
+    void addCoins(int amount);
     bool pay(int amount);
-    int getAttackStrength();
+    int getAttackStrength() const;
 };
 
 
