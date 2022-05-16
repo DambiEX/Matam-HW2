863e187901baece0d3101acd7319ff8251bd5c8a
anna_vashenko <annavashenko1@gmail.com>
2022-05-16 18:22:27 +0300

    submitted hw2.zip, added submssion certificate files

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -2,7 +2,6 @@
 
 
 Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards)
-<<<<<<< HEAD
         : m_cards_num(numOfCards), m_player(playerName) {
     const int INDEX_AT_BEGINNING=-1;
     m_status = GameStatus::MidGame;
@@ -13,25 +12,6 @@ Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCard
 void Mtmchkin::updateGameStatus() {
     const int MAX_LEVEL = 10;
     if (m_player.isKnockedOut()) {
-=======
-                    : m_cards_num(numOfCards), m_player(playerName) {
-    m_cards = new Card[numOfCards];
-    m_status = GameStatus::MidGame;
-    m_cards_index = 0;
-
-    for (int i = 0; i < numOfCards; ++i) {
-        m_cards[i] = cardsArray[i];
-    }
-}
-
-Mtmchkin::~Mtmchkin() {
-    delete m_cards;
-}
-
-void Mtmchkin::updateGameStatus(){
-    if (m_player.isKnockedOut())
-    {
->>>>>>> b1e2805ef25d669a385bc969fa6e303460a92651
         m_status = GameStatus::Loss;
     }
     if (m_player.getLevel() == MAX_LEVEL) {
@@ -41,12 +21,9 @@ void Mtmchkin::updateGameStatus(){
 
 void Mtmchkin::playNextCard() {
     increment_cards_index();
-    if (m_cards_num > 0)
-    {
-        Card current_card(m_cards[m_cards_index]);
-        current_card.printInfo();
-        current_card.applyEncounter(m_player);
-    }
+    Card current_card = m_cards[m_cards_index];
+    current_card.printInfo();
+    current_card.applyEncounter(m_player);
     updateGameStatus();
     m_player.printInfo();
 }
@@ -68,11 +45,4 @@ void Mtmchkin::increment_cards_index() {
     if (m_cards_index >= m_cards_num) {
         m_cards_index = 0;
     }
-<<<<<<< HEAD
 }
-=======
-}
-
-
-
->>>>>>> b1e2805ef25d669a385bc969fa6e303460a92651
