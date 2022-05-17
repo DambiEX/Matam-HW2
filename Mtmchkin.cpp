99a44511f602303c4dc0958addc67bda3b088a5d
Dambi <matan.wink@gmail.com>
2022-05-17 09:37:07 +0300

    Having some trouble merging

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -1,37 +1,56 @@
-#include "Mtmchkin.h"
+//
+// Created by PC on 6/5/2022.
+//
 
+#include "Mtmchkin.h"
+#define MAX_LEVEL 10
 
 Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards)
         : m_cards_num(numOfCards), m_player(playerName) {
-    const int INDEX_AT_BEGINNING=-1;
+    m_cards = new Card[numOfCards];
     m_status = GameStatus::MidGame;
-    m_cards = cardsArray;
-    m_cards_index = INDEX_AT_BEGINNING;
+    m_cards_index = 0;
+
+    for (int i = 0; i < numOfCards; ++i) {
+        m_cards[i] = cardsArray[i];
+    }
 }
 
-void Mtmchkin::updateGameStatus() {
-    const int MAX_LEVEL = 10;
-    if (m_player.isKnockedOut()) {
+Mtmchkin::~Mtmchkin() {
+    delete m_cards;
+}
+
+void Mtmchkin::updateGameStatus(){
+    if (m_player.isKnockedOut())
+    {
         m_status = GameStatus::Loss;
     }
-    if (m_player.getLevel() == MAX_LEVEL) {
+    if (m_player.getLevel() == MAX_LEVEL)
+    {
         m_status = GameStatus::Win;
     }
 }
 
 void Mtmchkin::playNextCard() {
     increment_cards_index();
-    Card current_card = m_cards[m_cards_index];
-    current_card.printInfo();
-    current_card.applyEncounter(m_player);
+    if (m_cards_num > 0)
+    {
+        Card current_card(m_cards[m_cards_index]);
+        current_card.printInfo();
+        current_card.applyEncounter(m_player);
+    }
     updateGameStatus();
     m_player.printInfo();
 }
 
-bool Mtmchkin::isOver() const {
-    if (getGameStatus() != GameStatus::MidGame) {
+bool Mtmchkin::isOver() const{
+    GameStatus status = getGameStatus();
+    if (status == GameStatus::Win || status == GameStatus::Loss)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
@@ -42,7 +61,11 @@ GameStatus Mtmchkin::getGameStatus() const {
 
 void Mtmchkin::increment_cards_index() {
     m_cards_index++;
-    if (m_cards_index >= m_cards_num) {
+    if (m_cards_index >= m_cards_num)
+    {
         m_cards_index = 0;
     }
 }
+
+
+
